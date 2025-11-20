ac54c06759defd8505fcfd4aec78a9c623003df5
Jüri Kiin <Kiin.Juri@gmail.com>
2025-11-20 15:49:36 +0000

    You're not making any code changes..

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: "/footyformation",
+  basePath: '/footyformation',
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
